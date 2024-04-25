2b822018b791f689311199e734bb5c551712c3b2
lzy <674194901@qq.com>
2024-04-25 20:37:33 +0800

    [sml] add chi2 stats in jax

--- sml/feature_selection/emulations/chi2_emul.py
+++ sml/feature_selection/emulations/chi2_emul.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      https://www.apache.org/licenses/LICENSE-2.0
+#   http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,39 +13,61 @@
 # limitations under the License.
 import os
 import sys
-import unittest
+import time
 
 import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.feature_selection import chi2 as chi2_sklearn
 
-import spu.spu_pb2 as spu_pb2
-import spu.utils.simulation as spsim
-
+# Add the library directory to the path
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../'))
+import sml.utils.emulation as emulation
 from sml.feature_selection.univariate_selection import chi2
 
 
-class UnitTests(unittest.TestCase):
-    def test_chi2(self):
-        sim = spsim.Simulator.simple(
-            3, spu_pb2.ProtocolKind.ABY3, spu_pb2.FieldType.FM128
-        )
+def emul_Chi2(mode: emulation.Mode.MULTIPROCESS):
+    print("start chi2 stats emulation")
 
-        def proc(x, y, num_class, max_iter, compute_p_value):
-            chi2_stats, p_value = chi2(x, y, num_class, max_iter, compute_p_value)
-            return chi2_stats, p_value
-
-        # create dataset
+    def load_data():
         x, y = load_iris(return_X_y=True)
+        return x, y
+
+    def proc(x, y, num_class, max_iter, compute_p_value):
+        chi2_stats, p_value = chi2(x, y, num_class, max_iter, compute_p_value)
+        return chi2_stats, p_value
+
+    try:
+        # bandwidth and latency only work for docker mode
+        emulator = emulation.Emulator(
+            emulation.CLUSTER_ABY3_3PC, mode, bandwidth=300, latency=20
+        )
+        emulator.up()
+        # load data
+        x, y = load_data()
         label_lst = np.unique(y)
         num_class = len(label_lst)
         max_iter = 1
         compute_p_value = True
-        chi2_stats, p_value = spsim.sim_jax(sim, proc, static_argnums=(2, 3, 4))(
-            x, y, num_class, max_iter, compute_p_value
-        )
+        # compare with sklearn
+        start_time = time.time()
         sklearn_chi2_stats, sklearn_p_value = chi2_sklearn(x, y)
+        end_time = time.time()
+        print("=========================")
+        print(f"Running time in SKlearn: {end_time - start_time:.3f}s")
+        print("=========================")
+        # mark these data to be protected in SPU
+        X_spu, y_spu = emulator.seal(x, y)
+        # run
+        start_time = time.time()
+        chi2_stats, p_value = emulator.run(proc, static_argnums=(2, 3, 4))(
+            X_spu, y_spu, num_class, max_iter, compute_p_value
+        )
+        end_time = time.time()
+        print("=========================")
+        print(f"Running time in SPU: {end_time - start_time:.3f}s")
+        print("=========================")
+
+        # print chi2 stats
         print("Chi2 stats result:")
         print(chi2_stats)
         print("Sklearn chi2 stats result:")
@@ -63,7 +85,11 @@ class UnitTests(unittest.TestCase):
         assert np.allclose(
             p_value, sklearn_p_value, rtol=1.0e-5, atol=1.0e-2, equal_nan=True
         )
+    except Exception as e:
+        print(e)
+    finally:
+        emulator.down()
 
 
 if __name__ == "__main__":
-    unittest.main()
+    emul_Chi2(emulation.Mode.MULTIPROCESS)
--- sml/feature_selection/univariate_selection.py
+++ sml/feature_selection/univariate_selection.py
@@ -1,71 +1,199 @@
 import jax
 import jax.numpy as jnp
-import numpy as np
-from jax.scipy.stats.chi2 import sf
-from sklearn.datasets import load_iris
 
 
-def chi2(X, y, label_lst):
+def _sf(x, df, max_iter=3):
     """
-    Calculate the chi-squared statistic and p-value for feature independence testing.
+    Calculates the survival function (1 - CDF) of the chi-squared distribution.
+
+    Parameters
+    ----------
+    x : array-like, shape (n_quantiles,)
+        The quantiles at which to compute the survival function.
+
+    df : int
+        The degrees of freedom of the chi-squared distribution. Must be positive.
+
+    max_iter : int, default=3
+        The maximum number of iterations for the numerical computation.
+
+    Returns
+    -------
+    result : array-like, shape (n_quantiles,)
+        The survival function evaluated at the given quantiles.
+
+    Notes
+    -----
+    The `max_iter` parameter is critical for the numerical stability of the computation. A high value can lead to overflow or underflow errors. Consider using a higher precision data type or reducing `max_iter` to ensure stability.
+    """
+    if df <= 0:
+        raise ValueError("Domain error, df must be positive")
+    condlist = [x < 0, x == 0, jnp.logical_or(x < 1, x < df)]
+    choicelist = [
+        jnp.ones_like(x),
+        jnp.zeros_like(x),
+        1 - _igam(0.5 * df, 0.5 * x, max_iter),
+    ]
+    result = jnp.select(
+        condlist, choicelist, default=_igamc(df * 0.5, x * 0.5, max_iter)
+    )
+    return result
+
+
+def _igam(a, x, max_iter=3):
+    """
+    Computes the regularized lower incomplete gamma function using a power series expansion for a given shape parameter.
+
+    Parameters
+    ----------
+    a : int
+        The shape parameter of the gamma function.
+
+    x : array-like, shape (n_quantiles,)
+        The quantiles at which to compute the incomplete gamma function.
+
+    max_iter : int, default=3
+        The maximum number of iterations for the numerical computation.
+
+    Returns
+    -------
+    ans : array-like, shape (n_quantiles,)
+        The regularized lower incomplete gamma function evaluated at the given quantiles.
+
+    Notes
+    -----
+    The `max_iter` parameter is crucial for the numerical stability of the computation. A high value can lead to overflow or underflow errors. It is recommended to use a higher precision data type or reduce the `max_iter` value to avoid such issues.
+    """
+    ax = jnp.power(x, a) * jnp.exp(-x) * jnp.exp(-jax.lax.lgamma(a))
+    # Power series
+    r = jnp.full_like(x, a)
+    c = jnp.ones_like(x)
+    ans = jnp.ones_like(x)
+
+    def loop_body(_, val):
+        r, c, ans = val
+        r += 1.0
+        c *= x / r
+        ans += c
+        return (r, c, ans)
+
+    init_val = (r, c, ans)
+    _, _, ans = jax.lax.fori_loop(0, max_iter, loop_body, init_val)
+    return ans * ax / a
+
+
+def _igamc(a, x, max_iter=3):
+    """
+    Computes the complementary regularized lower incomplete gamma function using a continued fraction representation for a given shape parameter.
+
+    Parameters
+    ----------
+    a : int
+        The shape parameter of the gamma function.
+
+    x : array-like, shape (n_quantiles,)
+        The quantiles at which to compute the complementary of incomplete gamma function.
+
+    max_iter : int, default=3
+        The maximum number of iterations for the numerical computation.
+
+    Returns
+    -------
+    ans : array-like, shape (n_quantiles,)
+        The complementary regularized lower incomplete gamma function evaluated at the given quantiles.
+
+    Notes
+    -----
+    The `max_iter` parameter is crucial for the numerical stability of the computation. A high value can lead to overflow or underflow errors. It is recommended to use a higher precision data type or reduce the `max_iter` value to avoid such issues.
+    """
+    # Compute ax = x**a * exp(-x) / Gamma(a)
+    ax = jnp.power(x, a) * jnp.exp(-x) * jnp.exp(-jax.lax.lgamma(a))
+    y = jnp.ones_like(x) - a
+    z = x + y + 1.0
+    c = jnp.zeros_like(x)
+    pkm2 = jnp.ones_like(x)
+    qkm2 = jnp.full_like(x, a)
+    pkm1 = x + 1.0
+    qkm1 = z * x
+    ans = pkm1 / qkm1
+
+    # Using Continued Fraction
+    def loop_body(_, val):
+        (c, y, z, pkm1, pkm2, qkm1, qkm2), ans = val
+        c += 1.0
+        y += 1.0
+        z += 2.0
+        yc = y * c
+        pk = pkm1 * z - pkm2 * yc
+        qk = qkm1 * z - qkm2 * yc
+        r = pk / qk
+        ans = r
+        pkm2 = pkm1
+        pkm1 = pk
+        qkm2 = qkm1
+        qkm1 = qk
+        return (c, y, z, pkm1, pkm2, qkm1, qkm2), ans
+
+    params = (c, y, z, pkm1, pkm2, qkm1, qkm2)
+    init_val = (params, ans)
+    params, ans = jax.lax.fori_loop(0, max_iter, loop_body, init_val)
+    return ans * ax
+
+
+def chi2(X, y, n_classes, max_iter=3, compute_p_value=False):
+    """
+    Performs a chi-squared test for independence between features and class labels.
 
     Parameters
     ----------
     X : array-like, shape (n_samples, n_features)
-        The feature matrix from which to calculate the chi-squared statistic. Each row
-        represents a sample and each column a feature.
+        The input feature matrix where each row represents a sample and each column a feature.
 
-    y : array-like, shape (n_samples, n_classes)
-        The one-hot encoded target vector indicating the class membership of
-        each sample. Each row should correspond to the class labels of the sample in
-        X.
+    y : array-like, shape (n_samples,)
+        An array of class labels for the samples in `X`. Labels should be integers in the range [0, n_classes-1].
 
-    label_lst : list of str, length = n_classes
-        A list of class labels corresponding to the columns of y.
+    n_classes : int
+        The number of unique classes in the classification problem.
+
+    max_iter : int, default=3
+        The maximum number of iterations for the numerical computation of the p-value. This parameter is crucial for numerical stability and convergence.
+
+    compute_p_value : bool, default=False
+        Whether to compute the p-value alongside the chi-squared statistic.
 
     Returns
     -------
-    chi2_stats : array, shape (n_features,)
-        The chi-squared statistic for each feature, indicating the degree of
-        association between the feature and the class labels.
+    chi2_stats : array-like, shape (n_features,)
+        The chi-squared statistics computed for each feature.
 
-    p_value : array, shape (n_features,)
-        The p-value associated with each chi-squared statistic, which can be used
-        to test the null hypothesis that the features are independent of the
-        class labels.
+    p_value : array-like, shape (n_features,), optional
+        The p-values corresponding to each feature's chi-squared statistic. Returned only if `compute_p_value` is set to True.
+
+    Notes
+    -----
+    - The `max_iter` parameter should be set with consideration of the potential for numerical overflow or underflow. In cases where numerical stability is a concern, consider using a higher precision data type (e.g., Float128 if available) or reducing the `max_iter` value.
+    - The p value computation cost maybe expensive. For applications such as feature selection where only the chi-squared statistic is needed, it may be more efficient to set `compute_p_value` to False.
+    - The input `y` should be provided as a 1-dimensional array with class labels encoded as integers in the specified range.
     """
-    total_samples = X.shape[0]
-    num_feature = X.shape[1]
-    num_class = len(label_lst)
+
+    # one hot encoding
+    y = jnp.eye(n_classes)[y]
     X = jnp.array(X)
-    y = jnp.array(y)  # Ensure y is a one-hot encoded vector
+    y = jnp.array(y)
     feature_count = jnp.sum(X, axis=0)
     class_prob = jnp.mean(y, axis=0)
-    # observed = jnp.zeros((num_feature, num_class))
-    # expected = jnp.zeros((num_feature, num_class))
     # Calculate the observed frequency count
     observed = jnp.dot(X.T, y)
-    # Calculate the expected frequency count
     expected = jnp.outer(feature_count, class_prob)
     # Calculate the chi-squared statistic
     chi2_stats = (observed - expected) ** 2 / expected
     # Sum over class dimensions
     chi2_stats = jnp.nansum(chi2_stats, axis=1)
     # Degrees of freedom
-    df = num_class - 1
-    # Calculate the p-value for each feature
-    p_value = sf(chi2_stats, df=df)
+    df = n_classes - 1
+    p_value = None
+    if compute_p_value:
+        # Calculate the p-value for each feature
+        p_value = _sf(chi2_stats, df=df, max_iter=max_iter)
+        p_value = jnp.where(p_value < 0, 0, p_value)
     return chi2_stats, p_value
-
-
-if __name__ == '__main__':
-    x, y = load_iris(return_X_y=True)
-    label_lst = np.unique(y)
-    num_class = len(label_lst)
-    y = np.eye(num_class)[y]
-    chi2_stats, p_value = chi2(x, y, label_lst)
-    print(chi2_stats, p_value)
-    from sklearn.feature_selection import chi2 as chi2_sklearn
-
-    sklearn_chi2_stats, sklearn_p_value = chi2_sklearn(x, y)
-    print(sklearn_chi2_stats, sklearn_p_value)
